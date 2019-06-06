d3a2ef60d1f121905da7bb6b9d729e690b2b8977
Aaron <abaxter@lhac.com>
2019-06-06 13:21:50 -0500

    discupdate

--- bot.js
+++ bot.js
@@ -113,35 +113,44 @@ bot.on('message', function (user, userID, channelID, message, event) {
                     //logger.info(message.author.id);
                     if (bot.servers[event.d.guild_id].members[userID].roles.includes('585543893244837899')) {
                         var fnMatch = args.match(/(\-?\d{1,3}\d*\.?\d*\/){3,4}\-?\d{1,2}\d*\.?\d*/);
-                        var flightNumbers = fnMatch[0];
-                        var discName = args.toString().replace(',' + flightNumbers, '');
 
-                        var options = {
-                            method: 'GET',
-                            uri: 'https://' + auth.pixel5_api + '@api.pixel5.us/discbot/discupdate/' + discName.replace(",", "%20").trim() + '/' + flightNumbers,
-                            headers: {
-        						'User-Agent': 'Request-Promise'
-        					},
-                            json: true // Automatically stringifies the body to JSON
-                        };
+                        if (!fnMatch[0]) {
+                            bot.sendMessage({
+                                to: channelID,
+                                message: 'Invalid flight number format.',
+                            });
+                        }
+                        else {
+                            var flightNumbers = fnMatch[0];
+                            var discName = args.toString().replace(',' + flightNumbers, '');
 
-                        rp(options)
-                            .then(function (parsedBody) {
-                                logger.info(parsedBody);
-                                // POST succeeded...
-                                bot.sendMessage({
-                                    to: channelID,
-                                    message: 'Disc "' + discName + '" Updated.',
-                                });
-                            })
-                            .catch(function (err) {
-                                logger.info(err);
-                                // POST failed...
-                                bot.sendMessage({
-                                    to: channelID,
-                                    message: 'Update failed, "' + discName + '" may not exist.',
-                                });
-                        });
+                            var options = {
+                                method: 'GET',
+                                uri: 'https://' + auth.pixel5_api + '@api.pixel5.us/discbot/discupdate/' + discName.replace(",", "%20").trim() + '/' + flightNumbers,
+                                headers: {
+            						'User-Agent': 'Request-Promise'
+            					},
+                                json: true // Automatically stringifies the body to JSON
+                            };
+
+                            rp(options)
+                                .then(function (parsedBody) {
+                                    logger.info(parsedBody);
+                                    // POST succeeded...
+                                    bot.sendMessage({
+                                        to: channelID,
+                                        message: 'Disc "' + discName + '" Updated.',
+                                    });
+                                })
+                                .catch(function (err) {
+                                    logger.info(err);
+                                    // POST failed...
+                                    bot.sendMessage({
+                                        to: channelID,
+                                        message: 'Update failed, "' + discName + '" may not exist.',
+                                    });
+                            });
+                        }
                     }
                     else {
                         bot.sendMessage({
