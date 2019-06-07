4fb59503425ce453375523722a3fda3acc3589af
Aaron <abaxter@lhac.com>
2019-06-07 12:14:54 -0500

    mypdga

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ bot.on('message', function (user, userID, channelID, message, event) {
                     var noMatch = false;
 
                     if (fnMatch) {
-                        var db_user_id = fnMatch[0].match(/[0-9]+/);
+                        var db_user_id = fnMatch[0].match(/[0-9]+/);logger.info (db_user_id);
                         var options = {
         					uri: 'https://' + auth.pixel5_api + '@api.pixel5.us/discbot/pdga/' + db_user_id,
         					headers: {
