9f4b3f18580cf9275133c7a6d6845d3d50896256
Aaron <abaxter@lhac.com>
2019-06-13 11:41:47 -0500

    private commands

--- bot.js
+++ bot.js
@@ -4,6 +4,8 @@ var auth = require('./auth.json');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
+var ownerUserID = '138098206587355137';
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -27,16 +29,26 @@ bot.on('ready', function (event) {
             type: 2
           }
     });
-    bot.editNickname({
-    	serverID: '367736108525682700',
-    	userID: '585833915957379101',
-    	nick: 'DiscBot',
-    });
 });
 
 bot.on('message', function (user, userID, channelID, message, event) {
     if (channelID in bot.directMessages && message.substring(0,9) != '.dischelp') {
         // Direct Message handling
+        if (userID == ownerUserID) {
+            var args = message.substring(1).split(' ');
+            var cmd = args[0];
+
+            args = args.splice(1).toString();
+            switch(cmd) {
+                case '.botnick':
+                    bot.editNickname({
+                        serverID: '367736108525682700',
+                        userID: '585833915957379101',
+                        nick: message.replace('.botnick', ''),
+                    });
+                break;
+            }
+        }
     }
     else {
         // Our bot needs to know if it will execute a command
